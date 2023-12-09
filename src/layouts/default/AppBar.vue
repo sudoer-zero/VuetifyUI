f0d12d58e898cec9a9e72cd928c8e6c9f96c9e2c
Sudoer <sudoer.zero@gmail.com>
2023-12-09 11:28:29 +0300

    Add a logo and some cleanup.

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,22 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
-    </v-app-bar-title>
-  </v-app-bar>
+  <v-container>
+    <v-responsive class="align-center text-center">
+      <v-row class="d-flex align-center justify-center py-2">
+        <v-col cols="auto">
+          <v-btn @click="toggleTheme" color="accent">{{ theme.global.current.value.dark ? 'Light' : 'Dark' }}</v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
 <script setup>
-  //
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 </script>
+
