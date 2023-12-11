ec71a04e5a189264c1a1680c7f63dbcfc808654d
Sudoer <sudoer.zero@gmail.com>
2023-12-11 10:33:16 +0300

    Add the components page.

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Home.vue'),
       },
+      {
+        path: '/components',
+        name: 'Components',
+        component: () => import('@/views/Components.vue'),
+      }
     ],
   },
 ]
---
+++ src/views/Components.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+   <h1>Components</h1>
+  </v-container>
+</template>
