4b43c66704095acd5789f76fd768257f163d659f
Sudoer <sudoer.zero@gmail.com>
2023-12-11 10:31:02 +0300

    Minor design updates.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,31 @@
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <v-img height="300" src="@/assets/logo.svg" />
-      <h1 class="text-h2 font-weight-bold py-6">Vuetify UI</h1>
+      <h1 class="text-h2 font-weight-bold py-6">
+        Vuetify UI
+      </h1>
       <div class="py-14" />
       <v-row class="d-flex align-center justify-center">
         <v-col cols="auto">
-          <v-btn href="#" min-width="164" rel="noopener noreferrer" variant="text">
+          <v-btn @click="$router.push('/components')" min-width="164" rel="noopener noreferrer" variant="text"
+            color="primary">
             <v-icon icon="mdi-view-dashboard" size="large" start />
-
             Components
           </v-btn>
         </v-col>
 
         <v-col cols="auto">
-          <v-btn color="primary" href="#" min-width="228" rel="noopener noreferrer" size="x-large" variant="flat">
+          <v-btn color="primary" @click="$router.push('/components')" min-width="228" rel="noopener noreferrer"
+            size="x-large" variant="flat">
             <v-icon icon="mdi-speedometer" size="large" start />
-
             Get Started
           </v-btn>
         </v-col>
 
         <v-col cols="auto">
           <v-btn href="https://github.com/sudoer-zero/VuetifyUI" min-width="164" rel="noopener noreferrer" target="_blank"
-            variant="text">
+            variant="text" color="primary">
             <v-icon icon="mdi-github" size="large" start />
-
             Github Repo
           </v-btn>
         </v-col>
